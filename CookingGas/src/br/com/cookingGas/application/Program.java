31983ec395594a4b86235a4bb367a4dbae4c8946
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-21 21:16:44 -0300

    Menu Cadastro pessoa

--- CookingGas/src/br/com/cookingGas/application/Program.java
+++ CookingGas/src/br/com/cookingGas/application/Program.java
@@ -7,10 +7,38 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        int opcao = 0;
+        do {
+            opcao = 0;
+            System.out.println("1 - Registrar Cliente");
+            System.out.println("2 - Dados Cliente");
+            System.out.println("3 - Alterar dados Cliente");
+            System.out.println("4 - Deletar Cliente");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Registrar");
+
+                    break;
+                case 2:
+                    System.out.println("Dados");
+                    break;
+                case 3:
+                    System.out.println("Alterar");
+                    break;
+                case 4:
+                    System.out.println("Deletar");
+                    break;
+                case 9:
+                    System.out.println("Programa finalizado");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
 
 
-
-
+        } while (opcao != 9);
 
 
         sc.close();
